c4f194e2d82c10104cdd6ad04b7b824edf34fbe7
harsh <hrohilla2k4@gmail.com>
2024-03-01 21:28:55 +0530

    (v-1): Refactor code

--- RegisterUser.mjs
+++ RegisterUser.mjs
@@ -3,33 +3,27 @@ import fs from "node:fs"
 import { exit } from "node:process";
 import jFile from "jsonfile"
 
-console.log("* Need your file path to store information *")
-console.log("* Simply enter the file name if you want to store file in same directory *")
-console.log("* Enter whole new path for another directory *")
+const collectInfo = {
+    userName : question("What is your name? "),
+    userEmail: question("Enter your email: "),
+    userDob : question("What is your D.O.B: \u001b[31mEnter in (dd/mm/yyyy): \x1b[0m"), 
+    userPassword : question("Enter a password: "),
+    confirmPassword : question("Confirm the password: ")
+}
 
 const fileName = 'important_information.txt'
 
-let userName = question("What is your name? ");
-
-let userEmail = question("Enter your email ");
-
-let userDob = question("What is your dob \u001b[31mEnter in (dd/yy/mm)] ");
-
-let userPassword = question("Enter password you want ");
-
-let confirmPassword = question("Confirm the password by entering again: ");
-
-if(userPassword !== confirmPassword){
+if(collectInfo.userPassword !== collectInfo.confirmPassword){
     console.log("Password do not match, try again")
     exit();
 }
 
 const userDetails = {
-    userName,
-    userEmail,
-    userDob,
-    userPassword,
-    confirmPassword
+    userName: collectInfo.userName,
+    userEmail: collectInfo.userEmail,
+    userDob: collectInfo.userDob,
+    userPassword: collectInfo.userPassword,
+    confirmPassword: collectInfo.confirmPassword
 }
 
 jFile.readFile(`./Logs_And_Files/important_information.json`, (err,data)=>{
